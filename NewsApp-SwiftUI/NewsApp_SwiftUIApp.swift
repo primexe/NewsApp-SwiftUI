30b628b23603e466f1e735f445d91325cca33afb
Artem Polozhentsev <artem.polozhencev@gmail.com>
2023-03-07 11:18:15 +0200

    Initial Commit

---
+++ NewsApp-SwiftUI/NewsApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NewsApp_SwiftUIApp.swift
+//  NewsApp-SwiftUI
+//
+//  Created by Artem Polozhentsev on 07.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct NewsApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
